0b685650a1c3d24aec3541ad2672939ddf0e6ce4
tb724-star <tabibito724@gmail.com>
2026-02-04 23:23:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -152,8 +152,7 @@ if st.session_state.confirming and current_server is not None:
     "server": current_server,
     "result": st.session_state.pending_result,
     "point": st.session_state.pending_point,
-         }
-
+         
     # --- Undo用 ---
     "undo_team_score": st.session_state.team_score,
     "undo_opp_score": st.session_state.opp_score,
